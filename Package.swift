87a2c9da1b4cdaa049cc27e1281ef4cfc26a567a
AkshayUdupa <akshay@geotoll.com>
2024-12-16 14:09:23 +0530

    refined

--- Package.swift
+++ Package.swift
@@ -8,37 +8,24 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // The library product makes the framework available to other packages or apps.
         .library(
             name: "GeoTollFramework",
             targets: ["GeoTollFramework"]
         ),
     ],
     dependencies: [
-        // Add the SPM dependencies here without specifying branch or version
-        .package(url: "https://github.com/Alamofire/Alamofire.git"),
-        .package(url: "https://github.com/yourkarma/JWT.git"),
-        .package(url: "https://github.com/instacart/TrueTime.swift.git"),
-        .package(url: "https://github.com/rwbutler/Connectivity.git"),
+        // No dependencies here as they will be handled by CocoaPods
     ],
     targets: [
-        // Define the target for your framework.
         .target(
             name: "GeoTollFramework",
-            dependencies: [
-                "Alamofire",
-                "JWT",
-                "TrueTime",
-                "Connectivity",
-            ],
+            dependencies: [],
             path: "./iOS/Sources/GeoTollFramework.xcframework"
         ),
-        // Define a test target for your framework.
         .testTarget(
             name: "GeoTollFrameworkTests",
             dependencies: ["GeoTollFramework"],
-            path: ".iOS/Tests/GeoTollFrameworkTests"
+            path: "iOS/Tests/GeoTollFrameworkTests"
         ),
     ]
 )
-
