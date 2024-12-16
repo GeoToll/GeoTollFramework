7ba71ff4edb446e95c2ec7d4e373edcbdc7037da
AkshayUdupa <akshay@geotoll.com>
2024-12-16 15:19:17 +0530

    Added sample Swift project

---
+++ Sample/Sample Swift/Sample Swift/Sample-Swift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Sample-Swift-Bridging-Header.h
+//  Sample Swift
+//
+//  Created by Akshay Udupa on 16/12/24.
+//
+
+#ifndef Sample_Swift_Bridging_Header_h
+#define Sample_Swift_Bridging_Header_h
+
+#import "GeoTollFramework/GeoTollFramework.h"
+
+#endif /* Sample_Swift_Bridging_Header_h */
---
+++ Sample/Sample Swift/Sample Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample Swift
+//
+//  Created by Akshay Udupa on 16/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
